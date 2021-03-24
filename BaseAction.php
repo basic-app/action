879e55b94bede2c5a0d107a49d3ad70590d8f7b5
basic-app <dev@basic-app.com>
2021-03-24 12:54:45 +0300

    n/a

--- BaseAction.php
+++ BaseAction.php
@@ -15,9 +15,21 @@ abstract class BaseAction extends Controller implements ActionInterface
 
     abstract public function _remap($method, ...$params);
 
-    public function __construct(Controller $controller)
+    public function __construct(Controller $controller, array $params = [])
     {
         $this->controller = $controller;
+
+        foreach($params as $key => $value)
+        {
+            if (property_exists($this, $key))
+            {
+                $this->$key = $value;
+            }
+            else
+            {
+                throw new Exception('Unknown property: ' . $key);
+            }
+        }
     }
 
 }
